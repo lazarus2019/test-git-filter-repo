6b1b83f5720e8ffc0f5b2b185d1b2f05d6334f19
lazarus2019 <thaisonbk2020@gmail.com>
2025-09-22 09:59:10 +0700

    feat(env): type-safe

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_NODE_ENV:
+    | 'local'
+    | 'development'
+    | 'qa'
+    | 'production'
+    | 'test';
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
